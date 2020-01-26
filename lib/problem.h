19320038f71abae7a23780c30905a2826a79ebfe
Ugo Pattacini <ugo.pattacini@iit.it>
2020-01-26 10:56:10 +0100

    doc updated

--- lib/problem.h
+++ lib/problem.h
@@ -150,7 +150,7 @@ public:
     yarp::sig::Vector get_dN(const double t) const;
 
    /**
-    * Compute the total force and torque of the object under the action of F and input forces.
+    * Compute the total force and torque acting on the object due to F and input forces.
     * @param forces is the 2D vector of the inward forces.
     * @return a pair representing the total force and torque.
     */
